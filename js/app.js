e23b9d2e69c9419deac16d9b24a2d7499a2f6d22
Dize dev <axiozweb2022@gmail.com>
2024-06-14 20:34:46 -0500

    se agrego los estilos de la secion about y animaciones para cada secion

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const buttons = document.querySelectorAll('.card-buttons button');
     const card = document.querySelector('.card');
+    const sections = document.querySelectorAll('.card-section');
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
 
             [ ...buttons ].map(item => item.classList.remove('is-active'));
+            [ ...sections ].flatMap(sec => sec.classList.remove('is-active'));
+
             btn.classList.add('is-active');
 
             btn.getAttribute('data-section') !== '#about'
@@ -15,6 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 : card.classList.remove('is-active');
 
             card.setAttribute('data-state', btn.getAttribute('data-section'));
+
+            const section = document.querySelector(btn.getAttribute('data-section'));
+            section.classList.add('is-active')
         });
     });
 });
