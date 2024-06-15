b79a9097394b9bca6cd78118e7bd8a8e47cc65e0
Dize dev <axiozweb2022@gmail.com>
2024-06-14 19:29:06 -0500

    se agrego los estilos del card header y la interactividas al cambiar de pestaña enla tarjeta

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const buttons = document.querySelectorAll('.card-buttons button');
+    const card = document.querySelector('.card');
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+
+            [ ...buttons ].map(item => item.classList.remove('is-active'));
+            btn.classList.add('is-active');
+
+            btn.getAttribute('data-section') !== '#about'
+                ? card.classList.add('is-active')
+                : card.classList.remove('is-active');
+
+            card.setAttribute('data-state', btn.getAttribute('data-section'));
+        });
+    });
 });
